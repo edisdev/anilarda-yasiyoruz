f3d36dee04a7fdf5911a7294cc77abdb5c75026c
haticeedis <hedis285@gmail.com>
2018-04-25 23:59:41 +0300

    add scss

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,3 @@ export default {
   name: 'App'
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import '@/assets/scss/index.scss';
 import App from './App';
 import router from './router';
 import store from './store';
 
+// using bootstrap vue
 Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
